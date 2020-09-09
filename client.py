4c66b5761313e1fb35e1be04792fe8dc4cfa9214
Martin Cvijovic <cvijovicmartin998@gmail.com>
2020-09-09 19:48:10 +0200

    initial board state

--- client.py
+++ client.py
@@ -46,10 +46,22 @@ running = True
 # pygame window settings
 pygame.display.set_caption("Chess by Martin Cvijovic")
 pygame.init()
-screen =  pygame.display.set_mode((600, 500))
+screen =  pygame.display.set_mode((870, 870))
 
 boardImage = pygame.image.load('board.png') # background
 rookWhiteImage = pygame.image.load('rook_white.png')
+knightWhiteImage = pygame.image.load('knight_white.png')
+bishopWhiteImage = pygame.image.load('bishop_white.png')
+queenWhiteImage = pygame.image.load('queen_white.png')
+kingWhiteImage = pygame.image.load('king_white.png')
+pawnWhiteImage = pygame.image.load('pawn_white.png')
+
+rookBlackImage = pygame.image.load('rook_black.png')
+knightBlackImage = pygame.image.load('knight_black.png')
+bishopBlackImage = pygame.image.load('bishop_black.png')
+queenBlackImage = pygame.image.load('queen_black.png')
+kingBlackImage = pygame.image.load('king_black.png')
+pawnBlackImage = pygame.image.load('pawn_black.png')
 
 if myColor == COLOR_WHITE:
     myMove = True
@@ -57,9 +69,13 @@ if myColor == COLOR_WHITE:
 
 # start the server listener:
 
-initX = 5 # initial coordinates 
-initY = 5 
-delta = 58 # number of steps to move to the next field
+# initX = 5
+# initY = 5
+# delta = 58
+
+initX = 0 # initial coordinates 
+initY = 0 
+delta = 102 # number of steps to move to the next field
 
 while running == True:
     screen.fill((125,74,74))
@@ -68,13 +84,38 @@ while running == True:
     # add all the initial pieces
     # TODO : 3 arrays, x[i], y[i], piece[i]
     # test purposes:
+
     for i in range(0, 8):
         for j in range(0, 2):
-            screen.blit(rookWhiteImage, (initX + i*delta, initY + j*delta)) 
+            if j == 1:
+                screen.blit(pawnBlackImage, (initX + i*delta, initY + j*delta)) 
+            else:
+                if i == 0 or i == 7:
+                    screen.blit(rookBlackImage, (initX + i*delta, initY + j*delta)) 
+                if i == 1 or i == 6:
+                    screen.blit(knightBlackImage, (initX + i*delta, initY + j*delta)) 
+                if i == 2 or i == 5:
+                    screen.blit(bishopBlackImage, (initX + i*delta, initY + j*delta)) 
+                if i == 3:
+                    screen.blit(kingBlackImage, (initX + i*delta, initY + j*delta)) 
+                if i == 4:
+                    screen.blit(queenBlackImage, (initX + i*delta, initY + j*delta))
 
     for i in range(0, 8):
         for j in range(6, 8):
-            screen.blit(rookWhiteImage, (initX + i*delta, initY + j*delta))
+            if j == 6:
+                screen.blit(pawnWhiteImage, (initX + i*delta, initY + j*delta))
+            else:
+                if i == 0 or i == 7:
+                    screen.blit(rookWhiteImage, (initX + i*delta, initY + j*delta)) 
+                if i == 1 or i == 6:
+                    screen.blit(knightWhiteImage, (initX + i*delta, initY + j*delta)) 
+                if i == 2 or i == 5:
+                    screen.blit(bishopWhiteImage, (initX + i*delta, initY + j*delta)) 
+                if i == 3:
+                    screen.blit(kingWhiteImage, (initX + i*delta, initY + j*delta)) 
+                if i == 4:
+                    screen.blit(queenWhiteImage, (initX + i*delta, initY + j*delta))
 
     pygame.display.update()
     for event in pygame.event.get():
